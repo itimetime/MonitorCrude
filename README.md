77baf17f0dcd5743af5068f5ca98a190b894f020
itimetime <itimetime@qq.com>
2020-03-10 16:09:10 +0800

    version 1.2

---
+++ README.md
@@ -0,0 +1,33 @@
+## 纽约原油（美原油）监控助手
+
+#### 使用说明：
+
+1.安装python
+
+2.在命令窗口输入`pip install request`
+
+3.在命令窗口 cd 进项目文件夹
+
+4.打开config.py修改相关配置（邮箱刷新频率等）
+
+5.命令窗口输入命令 `python Mainactivity` 
+
+#### 功能说明：
+
+1.设置低价、高价提醒
+
+2.邮件提醒
+
+3.设置多个价格，价格触发后会发送多封邮件，把握价格走势
+
+4.默认为美元（数据来自新浪财经），支持汇率设置
+
+#### config配置文件说明：
+
+![image-20200310160206462](C:\Users\Colin\AppData\Roaming\Typora\typora-user-images\image-20200310160206462.png)
+
+#### 相关截图
+
+![image-20200310160639902](C:\Users\Colin\AppData\Roaming\Typora\typora-user-images\image-20200310160639902.png)
+
+![image-20200310160559248](C:\Users\Colin\AppData\Roaming\Typora\typora-user-images\image-20200310160559248.png)
--- config.py
+++ config.py
@@ -6,24 +6,23 @@
 @file: config.py
 @time: 3/9/2020/4:19 PM
 """
-# 原油价格的低价购入提醒，如不修改汇率需设置为美元，可设置多个，设置一个举例：Minimum_reminder = [27]
+# 原油价格的低价购入提醒，可设置多个，设置一个举例：Minimum_reminder = [27]
 #设置多个举例： Minimum_reminder = [27, 28.5, 29]
-Minimum_reminder = [28, 27]
+Minimum_reminder = [27]
 #原油价格高价卖出提醒
-Highest_reminder = [40]
+Highest_reminder = [32]
 #设置监控类型，0代表监控低价，1代表监控高价
 Reminder_type = 0
 
-
-#设置发送邮件的邮箱
+#设置发送邮件的邮箱，需登录邮箱开启smtp服务，密码非邮箱登录密码，是开启smtp服务后给到的密码
 Host_server = 'smtp.qq.com'
-Email = 'icktime@qq.com'
-Email_passwod = 'czizkaacaupwbbdf'
+Email = 'example@qq.com'
+Email_passwod = 'password'
 
 #设置接受邮箱,添加多个用逗号隔开
-Receiver = ['icktime@163.com']
+Receiver = ['example@qq.com']
 
-#原油价格刷新频率，以秒为单位
+#原油价格刷新时间间隔，以秒为单位
 Refresh_interval = 20
 
 #设置汇率货币，默认为 1, 如修改人民币，可修改 为 Currency = '人民币'  Exchange_rate = 6.9448
@@ -32,3 +31,5 @@ Exchange_rate = 1
 
 # Currency = '人民币'
 # Exchange_rate = 6.9448
+
+
